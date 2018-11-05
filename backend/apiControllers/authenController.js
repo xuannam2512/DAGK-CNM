6ab3b4adaa83eb3c0e05da89b935857d9260ff70
Nam Le <xuannam2512@gmai.com>
2018-11-05 15:43:44 +0700

    update noteApi

--- backend/apiControllers/authenController.js
+++ backend/apiControllers/authenController.js
@@ -1,44 +1,14 @@
-/**
- * test authentication api
- */
-
 var express = require('express');
-
 var router = express.Router();
-
 var authen = require('../repos/authenRepo');
-
-router.get('/', (req, res) => {
-    res.json({
-        "message": "Authen"
-    });
-});
-
-router.get('/refreshtoken', (req, res) => {
-    let refreshToken = authen.generateRefreshToken();
-    authen.updateRefreshToken(1, refreshToken)
-        .then((data) => {
-            console.log(data);
-            res.statusCode = 201;
-            res.json(refreshToken);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.statusCode = 500;
-            res.end();
-        })
-});
 
 router.post('/accesstoken', (req, res) => {
         authen.refreshAccessToken(req.body.refeshToken)
         .then((data)=>{
-                //  đăng nhập thành công
                 res.status = 201;
                 res.json({
                     "accesToken": data
                 });
-
-            
         })
         .catch((err)=>{
             console.log(err);
@@ -47,8 +17,5 @@ router.post('/accesstoken', (req, res) => {
         })
 
 });
-
-
-
 
 module.exports = router;
